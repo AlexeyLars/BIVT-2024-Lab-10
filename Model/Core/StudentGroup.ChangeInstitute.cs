c47589254fcda8ab1d8b1ade2cb52800e2d2343d
Alexey Lars <mariawhowells@gmail.com>
2025-05-15 20:39:59 +0300

    feat: add conditions into StudentGroup ChangeInstitute method
    Добавлены дополнительные условия для проверки входных данных в методе ChangeInstitute StudentGroup

--- Model/Core/StudentGroup.ChangeInstitute.cs
+++ Model/Core/StudentGroup.ChangeInstitute.cs
@@ -5,7 +5,10 @@ public partial class StudentGroup
     public void ChangeInstitute(InstituteBase newInstitute, EducationalProgram newProgram)
     {
         if(newProgram == null) throw new ArgumentNullException("New educational program cannot be null", nameof(newProgram));
+        if(newInstitute == null) throw new ArgumentNullException("Institute cannot be null", nameof(newInstitute));
         if(ReferenceEquals(newInstitute, Institute)) throw new InvalidOperationException("New program cannot be the same as the current");
+        if(!newInstitute.EducationalPrograms.Contains(newProgram)) throw new InvalidOperationException("New institute does not contains the specified educational program");
+        
         EducationalProgram = newProgram;
         Institute = newInstitute;
     }
