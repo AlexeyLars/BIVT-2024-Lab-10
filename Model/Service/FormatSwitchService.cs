cdc8f146e5977a4f7b2f8e8ef21d7eab455029e4
Alexey Lars <mariawhowells@gmail.com>
2025-05-17 13:49:10 +0300

    feat: add FormatSwitchService class
    Добавлен класс для работы с конвертированием файлов хранения данных

---
+++ Model/Service/FormatSwitchService.cs
@@ -0,0 +1,24 @@
+﻿using Model.Data;
+
+namespace Model.Service;
+
+public static class FormatSwitchService
+{
+    public static void SwitchFormat<T>
+        (string oldPath, SerializerBase<T> oldSerializer, string newPath, SerializerBase<T> newSerializer)
+    {
+        if(string.IsNullOrWhiteSpace(oldPath)) throw new ArgumentException("Old path cannot be empty", nameof(oldPath));
+        if(string.IsNullOrWhiteSpace(newPath)) throw new ArgumentException("New path cannot be empty", nameof(newPath));
+        
+        Directory.CreateDirectory(newPath);
+        Directory.CreateDirectory(oldPath);
+        
+        var files = Directory.GetFiles(oldPath);
+        foreach (var file in files)
+        {
+            var fileName = Path.GetFileName(file);
+            var baseName = Path.GetFileNameWithoutExtension(file);
+            // todo
+        }
+    }
+}
